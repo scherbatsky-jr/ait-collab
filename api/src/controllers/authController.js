0c1d683824fd14b070caec4129e8c67dd0b22c73
Sunil Prajapati <sunilpzpt@gmail.com>
2023-10-28 15:02:12 +0700

    chore: add routes for password reset

--- api/src/controllers/authController.js
+++ api/src/controllers/authController.js
@@ -23,3 +23,36 @@ exports.login = async (req, res) => {
     res.status(401).json({ message: 'Login failed.' });
   }
 };
+
+exports.requestPasswordReset = async (req, res) => {
+  const { username } = req.body;
+  const user = await User.findOne({ username });
+
+  if (!user) {
+    return res.status(404).json({ message: 'User not found' });
+  }
+
+  const token = await authService.generateResetToken(user);
+
+  // Send the reset token to the user via email or other means
+  // You can implement your logic to send the token.
+
+  res.json({ message: 'Password reset token sent successfully' });
+};
+
+exports.resetPassword = async (req, res) => {
+  const { token } = req.params;
+  const { newPassword } = req.body;
+
+  const resetToken = await authService.verifyResetToken(token);
+
+  if (!resetToken) {
+    return res.status(400).json({ message: 'Invalid or expired token' });
+  }
+
+  const user = await User.findById(resetToken.userId);
+  await authService.resetPassword(user, newPassword);
+
+  res.json({ message: 'Password reset successful' });
+};
+
--- api/src/services/authService.js
+++ api/src/services/authService.js
@@ -1,4 +1,6 @@
 const User = require('../models/user');
+const ResetToken = require('../models/resetToken');
+
 const jwt = require('jsonwebtoken');
 
 require('dotenv').config();
@@ -32,4 +34,27 @@ exports.authenticateUser = async (username, password) => {
 
 exports.issueAccessToken = (user) => {
   return generateAccessToken(user);
+};
+
+exports.generateResetToken = async (user) => {
+// Generate a password reset token and save it in the database
+const token = require('crypto').randomBytes(32).toString('hex');
+const resetToken = new ResetToken({ userId: user._id, token });
+await resetToken.save();
+return token;
+};
+
+exports.verifyResetToken = async (token) => {
+// Verify if the reset token exists and is valid
+return ResetToken.findOne({ token });
+};
+
+exports.resetPassword = async (user, newPassword) => {
+// Reset the user's password
+user.setPassword(newPassword, (err) => {
+    if (err) {
+    throw err;
+    }
+    user.save();
+});
 };
